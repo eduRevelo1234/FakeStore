3683e8b2a1ee491d9428ac1812db5c0570113e06
Eduardo Revelo <eduardo.revelo@hotmail.com>
2024-05-05 18:59:36 -0500

    categories

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Pressable, FlatList, ScrollView,View,Text } from 'react-native';
-import React,{useState, useEffect} from 'react';
+import { StyleSheet, Pressable, FlatList, ScrollView, View, Text, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function Categories({route, navigation}){
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const url = "https://api.escuelajs.co/api/v1/categories"
+    useEffect(() => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((json) => setCategories(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+        console.log(userInfo)
+    }, []);
     return(
         <View>
            <Text> Categories</Text>
+
         </View>
     )
 }
